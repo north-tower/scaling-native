43d3c3362a845ed1bd41afc6b62201099b2109b6
north-tower <muneshmyke@gmail.com>
2024-03-23 00:28:56 +0300

    98

--- App.js
+++ App.js
@@ -7,8 +7,9 @@ import { NavigationContainer } from "@react-navigation/native"
 export default function App() {
   return(  
     <NavigationContainer>
-       <StackNavigator />
-
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
     </NavigationContainer>
   )
 }
---
+++ hooks/useAuth.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const AuthProvider = () => {
+  return (
+    <View>
+      <Text>AuthProvider</Text>
+    </View>
+  )
+}
+
