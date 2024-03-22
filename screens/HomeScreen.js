d4a99290b628eadbb432152944924d5eafedb1cf
north-tower <muneshmyke@gmail.com>
2024-03-23 00:27:09 +0300

    OOI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import {useNavigation} from "@react-navigation/native"
 
 const HomeScreen = () => {
+    const navigation = useNavigation()
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button title='Go to chat' onPress={() => navigation.navigate("Chat") } />
     </View>
   )
 }
